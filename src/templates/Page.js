7e34f8d9335f04d1dc427a0d2ae726072e8e4e7b
Kevin <kevin@kmcaloon.com>
2020-05-06 13:06:43 -0400

    Cleanup template directory

--- src/templates/Page.js
+++ src/templates/Page.js
@@ -1,34 +1,14 @@
-// import React from 'react';
-// import { useGroqQuery } from '../../plugins/gatsby-plugin-groq';
-//
-//
-// export const groqQuery = `{
-//   "page": *[ _type == "page" && _id == $_id ] {
-//     slug,
-//     title,
-//   }[0]
-// }`;
-// export const Page = ( { pageContext } ) => {
-//
-//   // const page = pageContext;
-//   //dfdfd
-//   // console.log( page );
-//   // console.log( data );
-//   const { page } = pageContext.data;
-//   console.log( pageContext.data.page );
-//   // const groqQuery = useGroqQuery( `{
-//   //   "something": *[ _type == "page" ] {
-//   //     ...,
-//   //     title,
-//   //   }[0]
-//   // }` );
-//
-//   return(
-//
-//     <div>
-//       <h1>{ page.title }</h1>
-//     </div>
-//   )
-//
-// }
-// export default Page;
+import React from 'react';
+
+export const Page = ( { pageContext } ) => {
+
+
+  return(
+
+    <div>
+      <h1>Try to add a groQuery export to this page!</h1>
+    </div>
+  )
+
+}
+export default Page;
